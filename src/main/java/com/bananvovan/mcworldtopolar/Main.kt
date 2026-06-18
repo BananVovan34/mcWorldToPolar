c1cc0efa60ad381e2822b01b5ca0bb823d560e38
BananVovan34 <ivavladimir777@gmail.com>
2026-06-19 02:07:13 +0300

    FlatLaf

--- src/main/java/com/bananvovan/mcworldtopolar/Main.kt
+++ src/main/java/com/bananvovan/mcworldtopolar/Main.kt
@@ -1,9 +1,13 @@
 package com.bananvovan.mcworldtopolar
 
 import com.bananvovan.mcworldtopolar.module.gui.MainFrame
+import com.formdev.flatlaf.FlatDarkLaf
 import javax.swing.SwingUtilities
+import javax.swing.UIManager
 
 fun main() {
+    UIManager.setLookAndFeel(FlatDarkLaf())
+
     SwingUtilities.invokeLater {
         MainFrame()
     }
